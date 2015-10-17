a71123738efeb117358f1f0ad7684c1affaa80c9
Quint Guvernator <quintus.public@gmail.com>
2015-10-17 20:45:59 +0200

    I hate the arduino default IDE

---
+++ examples/itsaunixsystem/itsaunixsystem.ino
@@ -0,0 +1,18 @@
+#include <Sixteen.h>
+
+#define DATAPIN  2
+#define CLOCKPIN 3
+#define RANDPIN  0 // n.b. this is an analog pin
+#define rchar random(display.firstChar, display.lastChar + 1)
+
+Sixteen display = Sixteen(DATAPIN, CLOCKPIN);
+void setup() {
+  // just for show; doesn't need to be secure
+  randomSeed(analogRead(0));
+}
+void loop() {
+  do {
+    display.writeChar(rchar, rchar);
+    delay(10);
+  } while (true);
+}
