4dcfd84bec92f784717aa0582075a4dc1bc3e101
Quint Guvernator <quintus.public@gmail.com>
2015-10-18 17:24:51 +0200

    style

--- examples/helloworld/helloworld.ino
+++ examples/helloworld/helloworld.ino
@@ -4,9 +4,11 @@
 #define CLOCKPIN 3
 
 Sixteen display = Sixteen();
+
 void setup() {
   display.addScreen(DATAPIN, CLOCKPIN);
 }
+
 void loop() {
   display.scroll("Hello world.", 150);
   display.scroll("In Zweden vocht groepje quakers b! sexfilm.   Pack my box with five dozen liquor jugs.   Portez ce vieux bon whiskys au juge blond qui fume.", 150);
