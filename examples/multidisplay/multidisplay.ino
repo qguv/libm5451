688817471d15947663323643419d1696a5afbcd7
Quint Guvernator <quintus.public@gmail.com>
2015-10-19 02:15:03 +0200

    use Sebastius' led pins in the example
    they're arbitrary anyway, right?

--- examples/multidisplay/multidisplay.ino
+++ examples/multidisplay/multidisplay.ino
@@ -1,18 +1,22 @@
 #include <Sixteen.h>
 
-#define DATAPINLEFT   2
-#define CLOCKPINLEFT  3
-#define DATAPINRIGHT  4
-#define CLOCKPINRIGHT 5
+#define DATAPINLEFT   A3
+#define CLOCKPINLEFT  A2
+#define DATAPINMID    A1
+#define CLOCKPINMID   A0
+#define DATAPINRIGHT  11
+#define CLOCKPINRIGHT 10
 
 Sixteen display = Sixteen();
 
 void setup() {
   display.addScreen(DATAPINLEFT, CLOCKPINLEFT);
+  display.addScreen(DATAPINMID, CLOCKPINMID);
   display.addScreen(DATAPINRIGHT, CLOCKPINRIGHT);
 }
 
 void loop() {
-  display.scroll("Hello world.", 150);
-  display.scroll("In Zweden vocht groepje quakers b! sexfilm.   Pack my box with five dozen liquor jugs.   Portez ce vieux bon whiskys au juge blond qui fume.", 150);
+  display.scroll("Hello world.", 100);
+  display.scroll("(also Sebastius)", 75);
+  display.scroll("In Zweden vocht groepje quakers b! sexfilm.   Pack my box with five dozen liquor jugs.   Portez ce vieux bon whiskys au juge blond qui fume.", 100);
 }
